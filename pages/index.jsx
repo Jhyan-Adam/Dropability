93ff7ab70a756052966e150972e399660b14055e
Jhyan-Adam <48757732+Jhyan-Adam@users.noreply.github.com>
2022-07-11 23:40:19 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { ActionIcon, Button, Image } from '@mantine/core';
+import { ActionIcon, Button, Image, Text } from '@mantine/core';
 //import MenuButton from "../public"
 //import TitleFrame from "../components/TitleFrame";
 
@@ -8,62 +8,125 @@ export default function HomePage() {
 
   function Title() {
     return (
-      <div
-        style={{
-          backgroundColor: "#FFFFFF",
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr 1fr",
-          justifyContent: "center",
-          padding: "1%",
-          width: "fill-container",
-          height: "wrap-content",
-        }}>
-        <h1
-          style={{
-            fontSize: "300%",
-            color: "cornflowerblue",
-            fontStyle: "normal",
-            fontWeight: "200",
-            lineHeight: "0",
-            letterSpacing: "0.15em",
-            textAlign: "center",
-            gridColumn: "2",
-          }}>
-          Dropability
-        </h1>
-
+      <>
         <div
           style={{
             display: "flex",
-            justifyContent: "flex-end",
-            alignSelf: "center"
+            flexDirection: "column",
+            alignItems: "center",
+            height: "100vh",
+            width: "100vw",
+            background: "#F8F8F8",
           }}>
-          <Link href="" passHref>
-            <Button
-              component="a"
-              styles={(theme) => ({
-                root: {
-                  backgroundColor: '#FAFAFA',
-                  //color: '#858585',
-                  border: 0,
-                  height: "wrapContent",
-                  width: "wrapContent",
-                  paddingLeft: 30,
-                  paddingRight: 30,
-                  paddingTop: 30,
-                  paddingBottom: 30,
-                  boxShadow: "2px 4px 4px rgba(0, 0, 0, 0.25)",
+          <div
+            style={{
+              width: "100vw",
+              height: "wrapContent",
+              backgroundColor: "#FFFFFF",
+              flex: "none",
+              order: "0",
+              alignSelf: "stretch",
+              flexGrow: "0",
+              display: "grid",
+              gridTemplateColumns: "1fr 1fr 1fr",
+              justifyContent: "center",
+              padding: "1%",
+            }}>
+            <h1
+              style={{
+                fontSize: "300%",
+                color: "cornflowerblue",
+                fontStyle: "normal",
+                fontWeight: "200",
+                lineHeight: "0",
+                letterSpacing: "0.15em",
+                textAlign: "center",
+                gridColumn: "2",
+              }}>
+              Dropability
+            </h1>
 
-                  '&:hover': {
-                    backgroundColor: theme.fn.darken('#FAFAFA', 0.05),
-                  },
-                }
-              })}>
-            </Button>
-          </Link>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                alignSelf: "center"
+              }}>
+              <Link href="" passHref>
+                <Button
+                  component="a"
+                  styles={(theme) => ({
+                    root: {
+                      backgroundColor: '#FAFAFA',
+                      color: '#858585',
+                      border: 0,
+                      height: "wrapContent",
+                      width: "wrapContent",
+                      //padding: "1%",
+                      paddingLeft: 30,
+                      paddingRight: 30,
+                      paddingTop: 30,
+                      paddingBottom: 30,
+                      boxShadow: "2px 4px 4px rgba(0, 0, 0, 0.25)",
+
+                      '&:hover': {
+                        backgroundColor: theme.fn.darken('#FAFAFA', 0.05),
+                      },
+                    }
+                  })}>
+                </Button>
+              </Link>
+            </div>
+          </div >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "10px",
+              gap: "10px",
+            }}>
+            <Text
+              style={{
+                fontFamily: "Dosis, sans-serif",
+                fontWeight: 400,
+                fontSize: "200%",
+                height: "wrapContent",
+                width: "wrapContent",
+              }}>
+              Tap to Search
+            </Text>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              padding: "0px 60px 60px",
+              gap: "50px",
+            }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "flex-start",
+                gap: "50px"
+              }}>
+              <Button
+                style={{
+                  width: "80px",
+                  height: "80px",
+                  background: "#1CE3CB",
+                  boxShadow: "0px 6px 14px rgba(0, 0, 0, 0.25)",
+                }}>
+
+              </Button>
+            </div>
+          </div>
         </div>
-        <image />
-      </div >
+      </>
     );
   }
 
@@ -78,7 +141,7 @@ export default function HomePage() {
   }
 
   return (
-    <Title/>
+    <Title />
   );
 
 }
