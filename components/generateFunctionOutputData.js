c7c84c6c01935a3694ea84eb71c1b4a4cb70f9b7
Jhyan <jhyanadam@outlook.com>
2024-07-14 11:12:33 +0200

    Deleted Proof of Concept File (For Reusable Code)

--- components/generateFunctionOutputData.js
+++
@@ -1,8 +0,0 @@
-export function generateCDFOutputData(inputNumber = 1000, pValue = 0.01) {
-    const functionOutput = [];
-    for (let index = 0; index < inputNumber; index++) {
-        functionOutput.push(1 - ((1 - pValue) ** index));
-    }
-
-    return functionOutput;
-};
